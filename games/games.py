b1b311f5c50fc7d32147cf7fbca20abbaf4906b0
CrowRoth <dustinrileywork@gmail.com>
2023-10-20 21:11:19 -0600

    edited games.py

--- games/games.py
+++ games/games.py
@@ -12,8 +12,8 @@ bp = Blueprint('games', __name__)
 def index():
     db = get_db()
     games = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
+        'SELECT g.title, console'
+        ' FROM games g JOIN user u ON g.player_id = u.id'
         ' ORDER BY created DESC'
     ).fetchall()
-    return render_template('games/index.html', posts=posts)
+    return render_template('games/index.html', games=games)

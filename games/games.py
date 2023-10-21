46636aad7901fc0e40e76e041eb83bff6c0ee2c3
CrowRoth <dustinrileywork@gmail.com>
2023-10-21 01:04:20 -0600

    added bootstrap

--- games/games.py
+++ games/games.py
@@ -14,6 +14,31 @@ def index():
     games = db.execute(
         'SELECT g.title, console'
         ' FROM games g JOIN user u ON g.player_id = u.id'
-        ' ORDER BY created DESC'
+        ' ORDER BY g.title DESC'
     ).fetchall()
     return render_template('games/index.html', games=games)
+
+@bp.route('/create', methods=('GET', 'POST'))
+@login_required
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        console = request.form['console']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO games (title, console, player_id)'
+                ' VALUES (?, ?, ?)',
+                (title, console, g.user['id'])
+            )
+            db.commit()
+            return redirect(url_for('games.index'))
+
+    return render_template('games/create.html')
